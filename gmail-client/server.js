cdac46115b1d89b350bf869e391526e65ef45203
Ammon Senkali Mugimu <amugimu@gmail.com>
2018-05-14 12:35:02 -0600

    Started the OAuth 2.0 authentication using vue-authenticate. Will need to finalize the token exchange done in the server.js file.

---
+++ gmail-client/server.js
@@ -0,0 +1,22 @@
+/*jshint esversion: 6 */
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+app.post('/auth/google', (req, res) => {
+    console.log("Recieved tokens");
+    res.self = res;
+    let responseBody = res.socket.parser.incoming.body;
+    // TODO Utilize the "code" attribute to finish the post
+    // authentication. Need to do more research on this.
+    console.log(responseBody);
+});
+
+app.listen(8082, () => console.log('Server listening on port 8082!'));
---
+++ gmail-client/src/components/GmailApi.vue
@@ -0,0 +1,57 @@
+<template>
+    <button @click="authenticate('google')">auth Google</button>
+</template>
+
+<script>
+import Vue from 'vue'
+import VueAxios from 'vue-axios'
+import VueAuthenticate from 'vue-authenticate'
+import axios from 'axios';
+
+Vue.use(VueAxios, axios)
+Vue.use(VueAuthenticate, {
+  baseUrl: 'http://localhost:8082/', // Your API domain
+  
+  providers: {
+    google: {
+      // Create a client ID and api key through the google 
+      // developer console.
+      clientId: 'XXXXXXXXXXXX-XXXXXXXXXXXXXXXX.apps.googleusercontent.com',
+      // Still figuring out the best use for the redirectUri
+      redirectUri: 'http://localhost:8080/',
+      // This is a scope for testing purposes. To be change to 
+      // one of the Gmail scopes.
+      scope: 'https://www.googleapis.com/auth/drive.metadata.readonly',
+    }
+  }
+})
+export default {
+  name: 'GmailApi',
+  data () {
+    return {
+      msg: ''
+    }
+  },
+  methods: {
+    login: function () {
+      this.$auth.login({ email, password }).then(function () {
+        // Execute application logic after successful login
+      })
+    },
+
+    register: function () {
+      this.$auth.register({ name, email, password }).then(function () {
+        // Execute application logic after successful registration
+      })
+    },
+    authenticate: function (provider) {
+      this.$auth.authenticate(provider).then(function (result) {
+        // Execute application logic after successful social authentication
+      })
+    }
+  }
+}
+</script>
+
+<style>
+</style>
