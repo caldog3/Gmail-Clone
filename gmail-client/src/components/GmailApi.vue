af3369780d9f71c0ea706bef7a87c2ac0942aa17
Calvin Woods <caldogwoods@byu.edu>
2018-05-30 17:45:57 -0600

    using a table layout and get the body through 'gmail-api-example' method

--- gmail-client/src/components/GmailApi.vue
+++ gmail-client/src/components/GmailApi.vue
@@ -1,19 +1,36 @@
 /* eslint-disable */
 <template>
+
   <div>
     <h1>Gmail Client</h1>
     <button @click="getMessages('google')">auth Google</button>
     
-    <ol id="example-1">
+    <table class="table table-striped table-inbox hidden">
+      <thead>
+        <tr>
+          <th>Sender</th>
+          <th>Subject</th>
+          <th>Snippet</th>
+        </tr>
+      </thead>
+      <tbody v-for="message in messages" :key="message.id">
+        <td> {{ message.from }}</td>
+        <td> {{ message.subject }}</td>
+        <td> {{ message.snippet }}</td>
+      </tbody>
+    </table>
+    <!-- <ol id="example-1">
     <li v-for="message in messages" :key="message.id">
       <hr style="height:.3em;background-color:#333;">
-      From: {{ message.from }} <b>|</b>
-      To: {{ message.to }} <b>|</b>
+      Sender: {{ message.from }} <b>|</b>
+      Recipient: {{ message.to }} <b>|</b>
       Subject: {{ message.subject }}
       <hr>
-      {{ message.snippet }}
+      Snippet: {{ message.snippet }}
+      <hr>
+      Body (decoded!): {{ message.body }}
     </li>
-  </ol>
+  </ol> -->
   </div>
 </template>
 
@@ -61,40 +78,82 @@ export default {
         }).then((messages)=>{
           console.log(messages);
           messages.forEach(message => {
-            this.getMessageSnippet(message.id);
+            this.getMessageContent(message.id);
           });
         }).catch((error) => {
           console.log(error);
         });
     },
-    getMessageSnippet(id){
+    getMessageContent(id){
         axios.get(`https://www.googleapis.com/gmail/v1/users/me/messages/${id}`, 
         { 
           headers: { 
             Authorization: `Bearer ${this.token}`
           }
         }).then((response) => {
-
+          console.log("MAIN RESPONSE JSON");
+          console.log(response);
           var headers = response.data.payload.headers;
-          console.log("THIS IS A HEADER");
-          console.log(headers);
-          for (var i = 0; i < headers.length; i++) {
-            if (headers[i].name === "From") {
-              var from = headers[i].value;
-            }
-            else if (headers[i].name === "Delivered-To") {
-              var to = headers[i].value;
-            }
-            else if (headers[i].name === "Subject") {
-              var subject = headers[i].value;
+          var isPromo = true;
+          for (var i = 0; i < response.data.labelIds.length; i++) {
+            let indexSpot = response.data.labelIds[i];
+            if (indexSpot === "CATEGORY_PROMOTIONS" || indexSpot === "CATEGORY_SOCIAL") {
+              isPromo = false;
             }
           }
-          let snippet = response.data.snippet;
-
-          this.messages.push({from, to, subject, snippet});
+          if (isPromo) {
+            for (var i = 0; i < headers.length; i++) {
+              if (headers[i].name === "From") {
+                var from = headers[i].value;
+              }
+              else if (headers[i].name === "Delivered-To") {
+                var to = headers[i].value;
+              }
+              else if (headers[i].name === "Subject") {
+                var subject = headers[i].value;
+              }
+            }
+            let snippet = response.data.snippet;
+            //////////////////
+            var message = response.data.payload;
+            var encodedBody = '';
+            if(typeof message.parts === 'undefined')
+            {
+              encodedBody = message.body.data;
+            }
+            else
+            {
+              encodedBody = this. getHTMLPart(message.parts);
+            }
+            encodedBody = encodedBody.replace(/-/g, '+').replace(/_/g, '/').replace(/\s/g, '');
+            let body = decodeURIComponent(escape(window.atob(encodedBody)));
+          
+            //Parts[1] is text with all the styling and tags / not sure how to use that though since
+               //it is all one string
+            //Parts[0] is plain text
+          //  let body = atob(response.data.payload.parts[0].body.data.replace(/-/g, '+').replace(/_/g, '/'));
+            this.messages.push({from, to, subject, snippet, body});
+          } 
         }).catch((error) => {
           console.log(error);
         });
+    },
+    getHTMLPart(arr) {
+      for(var x = 0; x <= arr.length; x++)
+      {
+        if(typeof arr[x].parts === 'undefined')
+        {
+          if(arr[x].mimeType === 'text/html')
+          {
+            return arr[x].body.data;
+          }
+        }
+        else
+        {
+          return getHTMLPart(arr[x].parts);
+        }
+      }
+      return '';
     },
     authenticate(provider){
       this.$auth.authenticate(provider)
@@ -104,7 +163,9 @@ export default {
     },
   }
 }
+
 </script>
 
 <style>
+
 </style>
