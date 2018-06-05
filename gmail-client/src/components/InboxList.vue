84f94c3ba2d96318dea428b78fe10b85d5ad962a
Calvin Woods <caldogwoods@byu.edu>
2018-06-05 09:39:54 -0600

    From and conciseFrom are sorted and stored

--- gmail-client/src/components/InboxList.vue
+++ gmail-client/src/components/InboxList.vue
@@ -4,23 +4,72 @@
     <b-tabs>
         <b-tab title="Primary">
             <div v-if="loadedMessage">
-                <ul id="example-1">
-                    <li v-for="message in messages" :key="message.from">
-                        {{ message.from }} |
-                        {{ message.subject }}|
-                        {{ message.snippet }}...|
-                        {{ message.time }}
-                    </li>
-                </ul>
+                <table class="table table-striped table-inbox hidden" id="example-1">
+                  <thead>
+                    <tr>
+                      <th>Sender</th>
+                      <th>Subject - Snippet</th>
+                      <th>Time</th>
+                    </tr>
+                  </thead>
+                    <tbody v-for="message in messages" :key="message.id">
+                        <td>{{ message.conciseFrom }}</td>
+                   <!-- <td>{{ message. body}}</td> -->
+                        <td>{{ message.subject }} - {{ message.snippet }}...</td>
+                        <td>{{ message.time }}</td>
+
+                    </tbody>
+                </table>
             </div>
             <div v-else>
                 <button type="submit" @click="getListOfMessages">Get Emails</button>
             </div>
         </b-tab>
         <b-tab title="Social">
-            
+            <div v-if="loadedMessage">
+              <table class="table table-striped table-inbox hidden" id="example-1">
+                <thead>
+                  <tr>
+                    <th>Sender</th>
+                    <th>Subject - Snippet</th>
+                    <th>Time</th>
+                  </tr>
+                </thead>
+                  <tbody v-for="socialM in socialMs" :key="socialM.id">
+                      <td>{{ socialM.conciseFrom }}</td>
+                 <!-- <td>{{ socialM. body}}</td> -->
+                      <td>{{ socialM.subject }} - {{ socialM.snippet }}...</td>
+                      <td>{{ socialM.time }}</td>
+
+                  </tbody>
+              </table>
+            </div>
+            <div v-else>
+              <p>"you don't have permission for this yet"</p>
+            </div>
         </b-tab>
         <b-tab title="Promotions">
+          <div v-if="loadedMessage">
+            <table class="table table-striped table-inbox hidden" id="example-1">
+              <thead>
+                <tr>
+                  <th>Sender</th>
+                  <th>Subject - Snippet</th>
+                  <th>Time</th>
+                </tr>
+              </thead>
+                <tbody v-for="promoM in promoMs" :key="promoM.id">
+                    <td>{{ promoM.conciseFrom }}</td>
+               <!-- <td>{{ promoM. body}}</td> -->
+                    <td>{{ promoM.subject }} - {{ promoM.snippet }}...</td>
+                    <td>{{ promoM.time }}</td>
+
+                </tbody>
+            </table>
+          </div>
+          <div v-else>
+            <p>"you don't have permission for this yet"</p>
+          </div>
         </b-tab>
     </b-tabs>
   </div>
@@ -33,8 +82,9 @@
 import Vue from 'vue'
 import VueAxios from 'vue-axios'
 import axios from 'axios';
-// import base64 from 'base-64';
+import base64 from 'base-64';
 
+Vue.use(require('vue-moment'))
 Vue.use(VueAxios, axios)
 
 export default {
@@ -42,7 +92,10 @@ export default {
   data () {
     return {
       messages: [],
+      socialMs: [],
+      promoMs: [],
       loadedMessage: false,
+      conciseFrom: false,
     }
   },
   methods: {
@@ -65,6 +118,7 @@ export default {
         });
     },
     getMessageContent(id){
+
         axios.get(`https://www.googleapis.com/gmail/v1/users/me/messages/${id}`, 
         { 
           headers: { 
@@ -74,32 +128,58 @@ export default {
           console.log("MAIN RESPONSE JSON");
           console.log(response);
           var headers = response.data.payload.headers;
-          var isPromo = true;
+          var isSocial = false;
+          var isPromo = false;
           for (var i = 0; i < response.data.labelIds.length; i++) {
             let indexSpot = response.data.labelIds[i];
-            if (indexSpot === "CATEGORY_PROMOTIONS" || indexSpot === "CATEGORY_SOCIAL") {
-              isPromo = false;
+            if (indexSpot === "CATEGORY_SOCIAL") {
+              isSocial = true;
+            }
+            if (indexSpot === "CATEGORY_PROMOTIONS") {
+              isPromo = true;
             }
           }
-          if (isPromo) {
-            for (var i = 0; i < headers.length; i++) {
-              if (headers[i].name === "From") {
-                var from = headers[i].value;
-              }
-              else if (headers[i].name === "Delivered-To") {
-                var to = headers[i].value;
-              }
-              else if (headers[i].name === "Subject") {
-                var subject = headers[i].value;
-              }
-              else if (headers[i].name === "Date") {
-                var time = headers[i].value;
-              }
+
+          for (var i = 0; i < headers.length; i++) {
+            if (headers[i].name === "From") {
+              var from = headers[i].value;
             }
-            let snippet = response.data.snippet;
-            let body = atob(response.data.payload.parts[0].body.data.replace(/-/g, '+').replace(/_/g, '/'));
-            this.messages.push({from, to, subject, snippet, body, time});
-          } 
+            else if (headers[i].name === "Delivered-To") {
+              var to = headers[i].value;
+            }
+            else if (headers[i].name === "Subject") {
+              var subject = headers[i].value;
+            }
+            else if (headers[i].name === "Date") {
+              var time = headers[i].value;
+            }
+          }
+          let snippet = response.data.snippet;
+          let id = response.data.id;
+          var body = "";
+          if (response.data.payload.parts === undefined) {
+            body = atob(response.data.payload.body.data.replace(/-/g, '+').replace(/_/g, '/'));
+          }
+          else {
+            body = atob(response.data.payload.parts[0].body.data.replace(/-/g, '+').replace(/_/g, '/'));
+          }
+
+
+          var conciseFrom = from.substr(0, from.indexOf('<'));
+          if (conciseFrom === ""){
+            conciseFrom = from;
+          }
+
+          if (!isSocial && !isPromo) {
+            this.messages.push({from, conciseFrom, to, subject, snippet, body, time, id});
+          }
+          if (isSocial) {
+            this.socialMs.push({from, conciseFrom, to, subject, snippet, body, time, id});
+          }
+          if (isPromo) {
+            this.promoMs.push({from, conciseFrom, to, subject, snippet, body, time, id});
+          }
+          console.log(moment().calendar());
         }).catch((error) => {
           console.log(error);
         });
