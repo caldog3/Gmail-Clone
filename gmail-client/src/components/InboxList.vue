66b9bd86ed6d08442327b12a880709b67b71959f
cate13 <claire.cate@yahoo.com>
2018-08-03 09:39:42 -0600

    patchy and hacky

--- gmail-client/src/components/InboxList.vue
+++ gmail-client/src/components/InboxList.vue
@@ -1,25 +1,55 @@
 
 /* eslint-disable */
 <template>
-  <div id="setWidth">
-    <b-tabs>
-      <b-tab title="Primary" v-on:click="updateCurrentFolder('PRIMARY')">
-        <email-list labelId="PRIMARY"/>
-      </b-tab>
-      <b-tab title="Social" v-on:click="updateCurrentFolder('SOCIAL')">
-        <email-list labelId="SOCIAL"/>
-      </b-tab>
-      <b-tab title="Promotions" v-on:click="updateCurrentFolder('PROMOTIONS')">
-        <email-list labelId="PROMOTIONS"/>
-      </b-tab>
-    </b-tabs>
+  <div>
+    <div id="setWidth">
+      <b-tabs>
+        <!-- <b-tab title="Primary" v-on:click="updateCurrentFolder('PRIMARY')"> -->
+        <b-tab>
+          <template slot="title">
+            <div class="backdrop">
+              Primary
+            </div>
+          </template>
+            <email-list labelId="PRIMARY"/>
+        </b-tab>
+        <!-- <b-tab title="Social" v-on:click="updateCurrentFolder('SOCIAL')"> -->
+        <b-tab>
+          <template slot="title">
+            <div class="backdrop">
+              Social
+            </div>
+          </template>
+          <email-list labelId="SOCIAL"/>
+        </b-tab>
+        <!-- <b-tab title="Promotions" v-on:click="updateCurrentFolder('PROMOTIONS')"> -->
+        <b-tab>
+          <template slot="title">
+            <div class="backdrop">
+              Promotions
+            </div>
+          </template>
+          <email-list labelId="PROMOTIONS"/>
+        </b-tab>
+      </b-tabs>
+    </div>
+
+
+    <!-- <div class="backdrop">
+
+    </div> -->
   </div>
 </template>
 
 <style scoped>
-/* #setWidth {
-  background: rgba(255, 255, 255, 0.6);
-} */
+#setWidth {
+  /* background: rgba(255, 255, 255, 0.6); */
+}
+.backdrop {
+  background-color: white;
+  width: 100%;
+  height: 100%;
+}
 </style>
 
 <script>
