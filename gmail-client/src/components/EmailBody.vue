570883d7252b95a9e58d37b398571e1caeb7cae5
Ammon S. Mugimu <amugimu@gmail.com>
2018-08-06 18:24:03 -0600

    Started Building Modals for attachments

--- gmail-client/src/components/EmailBody.vue
+++ gmail-client/src/components/EmailBody.vue
@@ -28,7 +28,17 @@
       
       <div v-if="message.attachmentIds.length > 0">
         <div v-for="attachmentId in message.attachmentIds" :key="attachmentId.attachmentId">
-          <img width="213" height="120" alt="An Image should be here" :src="`data:${attachments[attachmentId.attachmentId].mimeType};base64,${attachments[attachmentId.attachmentId].data}`"/>
+          <div class="attachment-container">
+            <div class="attachment">
+              <a href="#openModal">
+                <object :data="`data:${attachments[attachmentId.attachmentId].mimeType};base64,${attachments[attachmentId.attachmentId].data}`"/>
+              </a>
+              <div id="openModal" class="modalDialog">
+                <a href="#close" title="Close" class="close">X</a>
+                <object :data="`data:${attachments[attachmentId.attachmentId].mimeType};base64,${attachments[attachmentId.attachmentId].data}`"/>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -106,7 +116,72 @@ h4 {
   position: absolute;
   color:gold;
 }
+.attachment-container {
+  height: 120px;
+  width: 180px;
+}
+.attachment {
+  overflow: hidden;
+  border-style: solid;
+  border-width: 5px;
 
+  /* margin: auto; position: relative;  */
+  max-width: 100%; max-height: 100%;
+}
+/* object {
+    pointer-events: none;
+} */
+.modalDialog {
+	position: fixed;
+	font-family: Arial, Helvetica, sans-serif;
+	top: 0;
+	right: 0;
+	bottom: 0;
+	left: 0;
+	background: rgba(0,0,0,0.8);
+	z-index: 99999;
+	opacity:0;
+	-webkit-transition: opacity 400ms ease-in;
+	-moz-transition: opacity 400ms ease-in;
+	transition: opacity 400ms ease-in;
+	pointer-events: none;
+}
+.modalDialog:target {
+	opacity:1;
+	pointer-events: auto;
+}
+
+.modalDialog > div {
+	width: 400px;
+	position: relative;
+	margin: 10% auto;
+	padding: 5px 20px 13px 20px;
+	border-radius: 10px;
+	background: #fff;
+	background: -moz-linear-gradient(#fff, #999);
+	background: -webkit-linear-gradient(#fff, #999);
+	background: -o-linear-gradient(#fff, #999);
+}
+.close {
+	background: #606061;
+	color: #FFFFFF;
+	line-height: 25px;
+	position: absolute;
+	right: -12px;
+	text-align: center;
+	top: -10px;
+	width: 24px;
+	text-decoration: none;
+	font-weight: bold;
+	-webkit-border-radius: 12px;
+	-moz-border-radius: 12px;
+	border-radius: 12px;
+	-moz-box-shadow: 1px 1px 3px #000;
+	-webkit-box-shadow: 1px 1px 3px #000;
+	box-shadow: 1px 1px 3px #000;
+}
+
+.close:hover { background: #00d9ff; }
 </style>
 
 <script>
