5f179ae8c80ce72f1f3ec4eec14b8cadf579c825
Claire Cate <clairecate@localhost.localdomain>
2018-08-16 10:54:18 -0600

    what

--- gmail-client/src/components/InboxList.vue
+++ gmail-client/src/components/InboxList.vue
@@ -37,11 +37,202 @@
         <email-list labelId="PROMOTIONS"/>
       </b-tab>
 
+      <b-tab v-on:click="updateCurrentFolder('Private')">
+        <template slot="title">
+          <div class="backdrop">
+            <div class="tabSize">
+              <img src="./../assets/goldLock.png">
+              &emsp; Private
+            </div>
+          </div>
+        </template>
+        <div class="allPrivate">
+          <div class="privateInbox">
+            <div class="line">
+              <div class="img">
+                <img src="./../assets/UserNitaEllis.jpg">
+              </div>
+              <div class="snipTimeName">
+                <div class="snip">
+                  <b>Also, for each non-standard executable and testcase that clearly documents how it differs from the Program (independent of having been made by such Participant to </b>
+                </div>
+                <div class="timeName">
+                  <div>Nita Ellis</div>
+                  <div>Aug 13</div>
+                </div>
+              </div>
+            </div>
+            <div class="line">
+              <div class="img">
+                <img src="./../assets/UserAndrewGarrard.jpg">
+              </div>
+              <div class="snipTimeName">
+                <div class="snip">
+                  <b>You should also announce your new status</b>
+                </div>
+                <div class="timeName">
+                  <div>Andrew Garrard</div>
+                  <div>Aug 12</div>
+                </div>
+              </div>
+            </div>
+            <div class="line">
+              <div class="img">
+                <img src="./../assets/UserTylerAmjed.jpg">
+              </div>
+              <div class="snipTimeName">
+                <div class="snip">
+                  <b>We are a non-profit organization what you've given us is texty, we want sexy I think we need to start from scratch thanks for taking the time to make the website</b>
+                </div>
+                <div class="timeName">
+                  <div>Ty Amjed</div>
+                  <div>Aug 10</div>
+                </div>
+              </div>
+            </div>     
+            <div class="line">
+              <div class="img">
+                <img src="./../assets/UserKennethWoods.jpg">
+              </div>
+              <div class="snipTimeName">
+                <div class="snip">
+                  <b>Can you lower the price for the website? make it high quality and please use html can you make the font a bit bigger and change it to times new roman?</b>
+                </div>
+                <div class="timeName">
+                  <div>Ken Woods</div>
+                  <div>Aug 10</div>
+                </div>
+              </div>
+            </div>   
+            <div class="line">
+              <div class="img">
+                <img src="./../assets/UserLaurenShay.jpg">
+              </div>
+              <div class="snipTimeName">
+                <div class="snip">
+                  <b>If that Commercial Contributor in the hope that it becomes identical to those performance claims and warranties are such Commercial Contributor's responsibility alone. </b>
+                </div>
+                <div class="timeName">
+                  <div>Lauren Shay</div>
+                  <div>Aug 10</div>
+                </div>
+              </div>
+            </div>     
+            <div class="line">
+              <div class="img">
+                <img src="./../assets/UserAliceWalker.jpg">
+              </div>
+              <div class="snipTimeName">
+                <div class="snip">
+                  <b>Even if your work under the terms of this section is intended to, or may choose any version of the standards mode test cases </b>
+                </div>
+                <div class="timeName">
+                  <div>Alice</div>
+                  <div>Aug 8</div>
+                </div>
+              </div>
+            </div>   
+            <div class="line">
+              <div class="img">
+                <img src="./../assets/UserRichardTillman.jpg">
+              </div>
+              <div class="snipTimeName">
+                <div class="snip">
+                  <b>Remember, everything is the same or better you are lucky to even be doing this for us </b>
+                </div>
+                <div class="timeName">
+                  <div>Rich Tillman</div>
+                  <div>Aug 7</div>
+                </div>
+              </div>
+            </div>  
+            <div class="line">
+              <div class="img">
+                <img src="./../assets/UserJessicaDavis.jpg">
+              </div>
+              <div class="snipTimeName">
+                <div class="snip">
+                  <b>We need to have a Come to Jesus meeting with Phil about his attitude put your feelers out. </b>
+                </div>
+                <div class="timeName">
+                  <div>Jessie Davis</div>
+                  <div>Aug 5</div>
+                </div>
+              </div>
+            </div>  
+            <div class="line">
+              <div class="img">
+                <img src="./../assets/UserMarkShaine.jpg">
+              </div>
+              <div class="snipTimeName">
+                <div class="snip">
+                  <b>You may charge a reasonable royalty and payment arrangement are not compelled to copy the source form of the Work</b>
+                </div>
+                <div class="timeName">
+                  <div>Mark S</div>
+                  <div>Aug 5</div>
+                </div>
+              </div>
+            </div>  
+          </div>
+          <div class="selcted email">
+
+          </div>
+        </div>
+      </b-tab>
+
     </b-tabs>
   </div>
 </template>
 
 <style scoped>
+.img img {
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+}
+.img {
+  margin: 4px;
+}
+.timeName {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+.snip {
+  overflow: hidden;
+  white-space: nowrap;
+  width: 633px;
+  text-align: left;
+}
+.snipTimeName {
+  display: flex;
+  flex-direction: column;
+}
+.line {
+  display: flex;
+  flex-direction: row;
+  background-color: rgba(255, 255, 255, 0.6); 
+  margin: 4px 4px 0px 4px;
+  padding: 5px;
+}
+.privateInbox {
+  display: flex;
+  flex-direction: column;
+  width: 700px;
+  margin-bottom: 4px;
+}
+.allPrivate {
+  border: 10px solid white;
+  /* border-radius: 3px 3px 0px 0px; */
+  display: flex;
+  flex-direction: row;
+  
+}
+
+
+
+
 #setWidth {
   /* background: rgba(255, 255, 255, 0.6); */
   /* min-width: 600px; */
@@ -60,6 +251,11 @@
   height: 41px;
   line-height: 41px;
   text-align: center;
+}
+img {
+  width: 25px;
+  height: 20px;
+  margin-bottom: 5px;
 }
 </style>
 
