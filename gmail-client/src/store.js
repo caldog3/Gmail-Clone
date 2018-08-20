d4cb0b21547d8258b7e5818050fa3af5964acb25
Calvin Woods <caldogwoods@byu.edu>
2018-08-20 10:02:41 -0600

    adjusted query

--- gmail-client/src/store.js
+++ gmail-client/src/store.js
@@ -186,7 +186,7 @@ export default new Vuex.Store({
       gapi.client.load('gmail', 'v1').then(() => {
         gapi.client.gmail.users.threads.list({
           'userId': 'me',
-          'maxResults': 5,
+          'maxResults': 50,
           'q': query,
         }).then((response) => {
           console.log("query checkpoint 2");
