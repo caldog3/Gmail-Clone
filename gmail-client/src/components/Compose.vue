07f5d848f3c6e8fce84ff126bdceb4769b8fdf5b
Calvin <caldogwoods@gmail.com>
2019-05-20 12:42:06 -0600

    good checkpoint for uploads in case everything breaks

--- gmail-client/src/components/Compose.vue
+++ gmail-client/src/components/Compose.vue
@@ -25,6 +25,22 @@
       <quill-editor v-model="composeMessage"/>
     </div>
     
+    <!-- we'll just stick our upload button here for now -->
+    <form enctype="multipart/form-data" novalidate v-if="isInitial || isSaving">
+      <h1>Upload images</h1>
+      <div class="dropbox">
+        <input type="file" multiple :name="uploadFieldName" :disabled="isSaving" @change="filesChange($event.target.name, $event.target.files); fileCount = $event.target.files.length"
+          accept="image/*" class="input-file">
+          <p v-if="isInitial">
+            Drag your file(s) here to begin<br> or click to browse
+          </p>
+          <p v-if="isSaving">
+            Uploading {{ fileCount }} files...
+          </p>
+      </div>
+    </form>
+    
+    <!-- end upload button -->
     <div class="footerSection">
       <div class="sendButton">
         <input type="submit" class="SendButton1" value="Send" @click="sendCompose">
@@ -68,9 +84,63 @@ export default {
       existingDraft: false,
       draftId: "",
       threadId: "",
+
+      uploadedFiles: [],
+      uploadError: null,
+      currentStatus: null,
+      uploadFieldName: "photos",
+    }
+  },
+  computed: {
+    isInitial() {
+      return this.currentStatus === 'STATUS_INITIAL';
+    },
+    isSaving() {
+      return this.currentStatus === 'STATUS_SAVING';
+    },
+    isSuccess() {
+      return this.currentStatus === 'STATUS_SUCCESS';
+    },
+    isFailed() {
+      return this.currentStatus === 'STATUS_FAILED';
     }
   },
   methods: {
+    // uploader start
+    reset() {
+      // reset form to initial stater
+      this.currentStatus = 'STATUS_INITIAL';
+      this.uploadedFiles = [];
+      this.uploadError = null;
+    },
+    save(formData) { //might be out of the scope of our client
+      //upload data
+      this.currentStatus = 'STATUS_SAVING';
+      upload(formData)
+        .then(x => {
+          this.uploadedFiles = [].concat(x);
+          this.currentStatus = 'STATUS_SUCCESS';
+        })
+        .catch(err => {
+          this.uploadError = err.response;
+          this.currentStatus = 'STATUS_FAILED';
+        });
+    },
+    filesChange(fieldName, fileList) {
+      // handle file changes
+      const formData = new FormData();
+      if (!fileList.length) return;
+
+      // append the files to FormData
+      Array
+        .from(Array(fileList.length).keys())
+        .map(x => {
+          formData.append(fieldName, fileList[x], fileList[x].name);
+        });
+      // save it
+      this.save(formData);
+    },
+    //uploader finish
     open() {
       this.active = true
       //need to clear values if this is a basic compose...
@@ -117,6 +187,9 @@ export default {
       const {headers, body} = setupEmailBody(this.composeSubject, this.composeTo, this.composeMessage, sender);
       updateDraft(headers, body, this.draftId, this.threadId);
     },
+  },
+  mounted() {
+    this.reset();
   },
   created() {
     eventBus.$on('BODY_CLICK', this.close)
@@ -290,5 +363,36 @@ textarea {
   padding: 3px;
   margin: 4px;
 }
+
+/* styling for the image uploader */
+  .dropbox {
+    outline: 2px dashed grey; /* the dash box */
+    outline-offset: -10px;
+    background: lightcyan;
+    color: dimgray;
+    padding: 10px 10px;
+    min-height: 200px; /* minimum height */
+    position: relative;
+    cursor: pointer;
+  }
+
+  .input-file {
+    opacity: 0; /* invisible but it's there! */
+    width: 100%;
+    height: 200px;
+    position: absolute;
+    cursor: pointer;
+  }
+
+  .dropbox:hover {
+    background: lightblue; /* when mouse over to the drop zone, change color */
+  }
+
+  .dropbox p {
+    font-size: 1.2em;
+    text-align: center;
+    padding: 50px 0;
+  }
+/* end image uploader styling */
 
 </style>
