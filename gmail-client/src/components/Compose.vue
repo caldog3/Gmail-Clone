53de008bfcc0578d39405dcfa94fbd8a8e3a70f5
Calvin <caldogwoods@gmail.com>
2019-05-22 11:10:37 -0600

    upload window fixed

--- gmail-client/src/components/Compose.vue
+++ gmail-client/src/components/Compose.vue
@@ -1,7 +1,7 @@
 /* eslint-disable */
 
 <template>
-  <div class="compose" v-if="active" @click.stop>
+  <div class="compose composeWindow" v-if="active" @click.stop>
     <div class="headerSection">
       <div class="head">
         <h2>New Message</h2>
@@ -33,7 +33,7 @@
     </div>
     <div v-else>
       <form enctype="multipart/form-data" novalidate v-if="isInitial || isSaving">
-        <h1>Upload images</h1>
+        <h1>Upload Images</h1>
         <div class="dropbox">
           <input type="file" multiple :name="uploadFieldName" :disabled="isSaving" @change="filesChange($event.target.name, $event.target.files); fileCount = $event.target.files.length"
             accept="image/*" class="input-file">
@@ -209,6 +209,7 @@ export default {
       this.composeSubject = '';
       this.composeMessage = '';
       this.reset();
+      this.uploading = false;
     },
     focusOnSection(section) {
       this.activeSection = section;
@@ -408,36 +409,48 @@ textarea {
   padding: 3px;
   margin: 4px;
 }
+.composeWindow {
+  /* margin-right: 22px; */
+  overflow: hidden;
+  border-bottom: solid;
+  border-color: rgba(255, 255, 255, 0.4);
+}
+.composeWindow:hover {
+  overflow-y: scroll;
+  /* margin-right: 10px; */
+}
 
 /* styling for the image uploader */
-  .dropbox {
-    outline: 2px dashed grey; /* the dash box */
-    outline-offset: -10px;
-    background: lightcyan;
-    color: dimgray;
-    padding: 10px 10px;
-    min-height: 200px; /* minimum height */
-    position: relative;
-    cursor: pointer;
-  }
+.dropbox {
+  outline: 2px dashed grey; /* the dash box */
+  outline-offset: -10px;
+  background: lightcyan;
+  color: dimgray;
+  padding: 10px 10px;
+  min-height: 200px; /* minimum height */
+  position: relative;
+  cursor: pointer;
+}
 
-  .input-file {
-    opacity: 0; /* invisible but it's there! */
-    width: 100%;
-    height: 200px;
-    position: absolute;
-    cursor: pointer;
-  }
+.input-file {
+  opacity: 0; /* invisible but it's there! */
+  width: 100%;
+  min-height: 200px;
+  margin-left: -140px; /* adjust the input location to match the visual box */
+  margin-top: -10px;
+  position: absolute;
+  cursor: pointer;
+}
 
-  .dropbox:hover {
-    background: lightblue; /* when mouse over to the drop zone, change color */
-  }
+.dropbox:hover {
+  background: lightblue; /* when mouse over to the drop zone, change color */
+}
 
-  .dropbox p {
-    font-size: 1.2em;
-    text-align: center;
-    padding: 50px 0;
-  }
+.dropbox p {
+  font-size: 1.2em;
+  text-align: center;
+  padding: 50px 0;
+}
 /* end image uploader styling */
 
 </style>
